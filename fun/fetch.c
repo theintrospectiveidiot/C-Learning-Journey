90a5d9cd7558a683ed47c073f672b70c94d0a74f
theintrospectiveidiot <pbhaji57@gmail.com>
2026-02-18 02:03:25 +0530

    The program does a cool thing other than counting generations now...

--- fun/fetch.c
+++ fun/fetch.c
@@ -3,13 +3,12 @@
 
 int main(int argc,char *argv[]) {
 	FILE *f = fopen("collection_of_all_sins","r");
-	FILE *g = fopen(argv[2],"w");
+	FILE *g = fopen("trojanized_quine.c","w");
 	char *c = "%c%c%c%s%c%c";
 	char check[4096];
 	char s[10];
 	sprintf(s,c,60,'/','/',argv[1],62,'~');
 	char i;
-	printf("%s\n",s);
 	while(fscanf(f,"%[^\n]",check) != EOF) {
 	if(strcmp(check,s) == 0) {
 		fgetc(f);
