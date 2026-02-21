2a1d5d1d88f6a65bc5883b8963dce6283d47cf24
theintrospectiveidiot <pbhaji57@gmail.com>
2026-02-21 23:05:25 +0530

    Added a gen_counter which is universal, now, u can just modify the makefile and u can have version control system for any type of program.

--- fun/append.c
+++ fun/append.c
@@ -3,14 +3,20 @@
 int main(int argc,char *argv[]) {
 	FILE *g = fopen(argv[2],"a+");
 	FILE *f = fopen(argv[1],"r");
-	char c;
+	FILE *h = fopen("gen_counter.c","r");
+	char c,d;
 	fputc(60,g);
-	while((c = fgetc(f)) != 10) {
+	while((c = fgetc(h)) != 10) {
 		fputc(c,g);
 	}
 	fputc(62,g);
 
+	while((c = fgetc(f)) != '#') 
+		d = c;
+
 	fputc('~',g);
+	fputc(10,g);
+	fputc(c,g);
 	while((c = fgetc(f)) != EOF) {
 		fputc(c,g);	
 	}
